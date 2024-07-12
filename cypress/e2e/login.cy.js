97929aacafa25f158a82f7b76d36aac4414a1479
audukehinde <auduomohkehinde@gmail.com>
2024-07-12 20:36:22 +0100

    Created a credential file in the fixture folder to store login details

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,4 @@
-const credentials = "credentials"
+let credentials
 
 
 describe('User login', () => {
@@ -10,16 +10,17 @@ describe('User login', () => {
     
     beforeEach(() => {
         cy.visit('/');
+        cy.fixture('credentials').then((creds) => {
+            credentials = creds; // Assign fixture data to the outer scope variable
+        });
     })
     it('Verify the user is able to login with valid credentials', () => {
         cy.get('.fa-user').should('exist').and('be.visible').click();
         cy.get('.dropdown-menu-right').should('exist').and('be.visible').contains('Login').click();
-        cy.get('#input-email').should('exist').and('be.visible').type('scandiumaccessment34@yopmail.com')
-        cy.get('#input-password').should('exist').and('be.visible').type('Scandium@123')
+        cy.get('#input-email').should('exist').and('be.visible').type(credentials.email)
+        cy.get('#input-password').should('exist').and('be.visible').type(credentials.password)
         cy.get("[type='submit']").should('exist').and('be.visible').click()
         cy.get('div#content h2:first').should('have.text', "My Account")
-
-
-
+        cy.url().should('include', '/account');
     })
 })
