c89c9d81b94ca31041a469f5a862978d2540463d
audukehinde <auduomohkehinde@gmail.com>
2024-07-22 21:17:24 +0100

    Added login page elements to an object

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -19,45 +19,45 @@ beforeEach(() => {
 
 // -- Valid login method  --
 Cypress.Commands.add('userValidLogin', () => {
-    cy.ClickElement(sel.account_button);
-    cy.get(sel.login_button).should('exist').and('be.visible').contains('Login').click();
-    cy.Inputelement(sel.emailField, credentials.validLogin.email);
-    cy.Inputelement(sel.passwordField, credentials.validLogin.password);
-    cy.ClickElement(sel.submit);
+    cy.clickElement(sel.loginPage.account_button);
+    cy.get(sel.loginPage.login_button).should('exist').and('be.visible').contains('Login').click();
+    cy.Inputelement(sel.loginPage.emailField, credentials.validLogin.email);
+    cy.Inputelement(sel.loginPage.passwordField, credentials.validLogin.password);
+    cy.clickElement(sel.loginPage.submit);
     cy.verifyUrl('/account');
     cy.logout();
 })
 
 // -- Invalid login method. Email is invalid and password is invalid  --
 Cypress.Commands.add('userInvalidLogin', () => {
-    cy.ClickElement(sel.account_button);
-    cy.get(sel.login_button).should('exist').and('be.visible').contains('Login').click();
-    cy.Inputelement(sel.emailField, credentials.invalidLogin.email);
-    cy.Inputelement(sel.passwordField, credentials.invalidLogin.password);
-    cy.ClickElement(sel.submit);
+    cy.clickElement(sel.loginPage.account_button);
+    cy.get(sel.loginPage.login_button).should('exist').and('be.visible').contains('Login').click();
+    cy.Inputelement(sel.loginPage.emailField, credentials.invalidLogin.email);
+    cy.Inputelement(sel.loginPage.passwordField, credentials.invalidLogin.password);
+    cy.clickElement(sel.loginPage.submit);
     cy.verifyUrl('/login');
 })
 
 // -- User clicks on the Login Button with empty fields --
 Cypress.Commands.add('userEmptyLogin', () => {
-    cy.ClickElement(sel.account_button);
-    cy.get(sel.login_button).should('exist').and('be.visible').contains('Login').click();
-    cy.Inputelement(sel.emailField, '');
-    cy.Inputelement(sel.passwordField, '');
-    cy.ClickElement(sel.submit);
+    cy.clickElement(sel.loginPage.account_button);
+    cy.get(sel.loginPage.login_button).should('exist').and('be.visible').contains('Login').click();
+    cy.Inputelement(sel.loginPage.emailField, '');
+    cy.Inputelement(sel.loginPage.passwordField, '');
+    cy.clickElement(sel.loginPage.submit);
     cy.verifyUrl('/login');
 })
 
 // -- Logout Method --
 Cypress.Commands.add('logout', () => {
-    cy.ClickElement(sel.account_button);
+    cy.clickElement(sel.loginPage.account_button);
     cy.get('ul.dropdown-menu.dropdown-menu-right').should('be.visible')
     .find('a').contains('Logout').click();
     cy.verifyUrl('/logout');
 })
 
 // -- This method clicks any button on the UI --
-Cypress.Commands.add('ClickElement',(ele) => {
+Cypress.Commands.add('clickElement',(ele) => {
     cy.get(ele).should('exist').and('be.visible').click();
 })
 
